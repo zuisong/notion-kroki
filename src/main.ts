d3d5a522840498ee82d1127f48f656216d1957a8
zuisong <com.me@foxmail.com>
2022-07-15 13:19:06 +0800

    change code

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,24 @@ const defaultConfig: KrokiOption = {
 };
 
 await sleep(3000); // wait document render end
-const blocks: HTMLElement[] = _xpath("//div[ contains( @class,'notion-code-block') and @data-block-id]", document.body);
-for (const block of blocks) {
-  if (_xpath(".//div[text()='Plain Text']", block).length == 0) {
-    continue;
-  }
+const blocks: HTMLElement[] = _xpath("//*[starts-with(text(),'//kroki ')]", document.body);
+for (const codeDiv of blocks) {
 
-  const codeDiv = _xpath(".//div[contains(@class,'notion-code-block')]", block).at(0);
+
   if (!codeDiv) continue;
 
   if (codeDiv.innerText.startsWith('//kroki')) {
     const lines = codeDiv.innerText.split('\n');
     const type = lines[0].replace('//kroki', '').trim();
+    if (!type?.trim()) continue
     const data = lines.filter((value, index) => index != 0).join('\n');
+    if (!data?.trim()) continue
     const obj = plant(data, type, defaultConfig);
     const div = document.createElement('div', undefined);
     div.style.cssText = 'display: flex; flex-direction: row; place-content: center;'
     div.innerHTML = obj;
 
-    codeDiv.parentElement?.appendChild(div);
+    codeDiv.parentElement?.parentElement?.appendChild(div);
   }
 }
 
@@ -41,7 +40,7 @@ function plant(content: string, type: string, config: KrokiOption) {
   const urlPrefix = `${config?.serverPath + type}/svg/`;
   const data: Uint8Array = textEncode(content);
   const compressed: string = strFromU8(zlibSync(data, { level: 9 }), true);
-  const result: string = btoa(compressed).replace(/\+/g, '-').replace(/\//g, '_');
+  const result: string = window.btoa(compressed).replace(/\+/g, '-').replace(/\//g, '_');
   const svgUrl: string = urlPrefix + result;
 
   return `<object type="image/svg+xml" data="${svgUrl}" />`;
