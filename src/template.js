5d418c654afd5423466b7d4fe0aa3180ba7f6e55
zuisong <com.me@foxmail.com>
2022-08-10 16:57:31 +0800

    add 'super.site' support

--- src/template.js
+++ src/template.js
@@ -4,6 +4,7 @@
 // @grant       none
 // @version     [version]
 // @match        *://www.notion.so/*
+// @match        *://*.super.site/*
 // @supportURL  https://github.com/zuisong/notion-kroki/issues
 // @run-at      document-idle
 // @author      zuisong
