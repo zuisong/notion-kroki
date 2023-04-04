b229fd43ed7a500cc72724662f3e712d17944adc
zuisong <com.me@foxmail.com>
2023-04-04 14:58:46 +0800

    use happy-dom-deno to test

--- test/common/jdsom-env-init.ts
+++ test/common/jdsom-env-init.ts
@@ -1,33 +1,17 @@
 import { delay } from "deno_std/async/delay.ts";
 import type {} from "$/src/@types/types.d.ts";
 import { requiredLibs } from "$/src/userscript-meta.ts";
-import { DOMParser } from "deno-dom-wasm";
+import { Window } from "npm:happy-dom-deno";
 
 export async function init() {
-  const doc = new DOMParser().parseFromString(
-    `
-    <h1>Hello World!</h1>
-    <p>Hello from <a href="https://deno.land/">Deno!</a></p>
-  `,
-    "text/html",
-  )!;
+  const win = new Window();
+  const doc = win.document;
+  doc.body.innerHTML = '<div class="container"></div>';
 
-  window.MutationObserver = MutationObserverImpl as unknown as any;
-
+  window.MutationObserver = win.MutationObserver as any;
   window.document = doc as unknown as Document;
   window.TextDecoder = undefined as any;
   await Promise.all(requiredLibs.map((lib) => import(lib)));
-}
-
-class MutationObserverImpl {
-  constructor(private callBack: MutationCallback, private o: MutationObserver) {
-  }
-
-  observe(target: Node, options?: MutationObserverInit): void {
-    setTimeout(() => {
-      this.callBack([undefined as any], undefined as any);
-    }, 300);
-  }
 }
 
 export async function tearDown() {
