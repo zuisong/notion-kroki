957babc833749738532aab28b71a77a306b6e542
zuisong <com.me@foxmail.com>
2023-07-07 17:56:03 +0800

    upgrade deps

--- test/common/dom-env-init.ts
+++ test/common/dom-env-init.ts
@@ -1,21 +1,20 @@
 import { delay } from "deno_std/async/delay.ts";
-import type {} from "$/src/@types/types.d.ts";
+import type { } from "$/src/@types/types.d.ts";
 import { requiredLibs } from "$/src/userscript-meta.ts";
-import { GlobalWindow } from "npm:happy-dom@9.20.3";
+import { GlobalRegistrator } from "npm:@happy-dom/global-registrator";
 
 export async function init() {
-  const win = new GlobalWindow();
-  const doc = win.document;
-  doc.body.innerHTML = '<div class="container"></div>';
+  //@ts-ignore
+  GlobalRegistrator.registered = []
+  GlobalRegistrator.register()
 
-  window.MutationObserver = win.MutationObserver as any;
-  window.document = doc as unknown as Document;
+  document.body.innerHTML = '<div class="container"></div>';
   window.TextDecoder = undefined as any;
   await Promise.all(requiredLibs.map((lib) => import(lib)));
 }
 
 export async function tearDown() {
-  let id = window.setTimeout(function () {}, 0);
+  let id = window.setTimeout(function () { }, 0);
   while (id--) {
     window.clearTimeout(id); // will do nothing if no timeout with id is present
   }
