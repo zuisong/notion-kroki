08766c69356d83e6eeea43771cc7eb2ccfe9dd8d
zuisong <com.me@foxmail.com>
2023-08-04 21:34:17 +0800

    update

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-import * as rollup from "esm.sh/rollup@3.26.3?bundle";
-import * as swc from "npm:@swc/wasm@1.3.70";
+import * as rollup from "esm.sh/rollup@3.27.1?bundle";
+import * as swc from "npm:@swc/wasm@1.3.74";
 import * as happyDom from "npm:happy-dom@10.5.2";
 
 export { happyDom, rollup, swc };
